ad444d17126128598259002cd2ceb86e038a8099
Sangita kumari <kumarisangita8800@gmail.com>
2026-08-03 14:21:20 +0530

    added clerk route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors';
 import 'dotenv/config';
 import connectDb from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 //initialize express 
 const app = express();
@@ -16,7 +17,8 @@ app.use(cors())
 //routes
 app.get('/', (req, res)=>{
   res.send("API working")
-})
+});
+app.post('/clerk', express.json(), clerkWebhooks);
 
 //Port
 const PORT = process.env.PORT || 5000;
