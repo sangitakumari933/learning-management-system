38f55844c9030a61b3e8eff1d7ca3b2f47b358d4
Sangita kumari <kumarisangita8800@gmail.com>
2026-08-04 09:10:28 +0530

    solved errors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import cors from 'cors';
-import 'dotenv/config';
-import connectDb from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
-//initialize express 
+//initialize express
 const app = express();
 
 //connect to database
@@ -12,17 +12,17 @@ await connectDb();
 
 //middlewares
 
-app.use(cors())
+app.use(cors());
 
 //routes
-app.get('/', (req, res)=>{
-  res.send("API working")
+app.get("/", (req, res) => {
+  res.send("API working");
 });
-app.post('/clerk', express.json(), clerkWebhooks);
+app.post("/clerk", express.json(), clerkWebhooks);
 
 //Port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-  console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
